0e9bb78498c4132bf5b03a4d8f51df1688c7d2e4
senal <ranga.fernando@gmail.com>
2021-09-26 18:35:11 +1000

    Use Formik, Field and ErrorMessage components to reduce number of repeated code

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 function YoutubeForm() {
@@ -20,35 +20,33 @@ function YoutubeForm() {
         channel: Yup.string().required('Required'),
     });
 
-    const formik = useFormik({
-        initialValues,
-        onSubmit: handleSubmit,
-        validationSchema
-    });
-
     return (
-        <div>
-            <form onSubmit={formik.handleSubmit} novalidate>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+            >
+            <Form >
                 <div className="form-control">
                     <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" {...formik.getFieldProps('name')}></input>
-                    {formik.errors.name && formik.touched.name ? <span className="error">{formik.errors.name}</span> : <></>}
+                    <Field type="text" id="name" name="name" ></Field>
+                    <ErrorMessage className='error' name='name'></ErrorMessage>
                 </div>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
-                    <input type="email" formnovalidate  id="email" name="email" {...formik.getFieldProps('email')}></input>
-                    {formik.errors.email  && formik.touched.email ? <span className="error">{formik.errors.email}</span> : <></>}
+                    <Field type="email"  id="email" name="email"></Field>
+                    <ErrorMessage name='email'></ErrorMessage>
                 </div>
                 <div className="form-control">
                     <label htmlFor="channel">Channel</label>
-                    <input type="text" id="channel" name="channel" {...formik.getFieldProps('channel')} ></input>
-                    {formik.errors.channel && formik.touched.channel ? <span className="error">{formik.errors.channel}</span> : <></>}
+                    <Field type="text" id="channel" name="channel"></Field>
+                    <ErrorMessage name='channel'></ErrorMessage>
                 </div>
                 <div className="form-control">
-                    <input type="submit" value="submit"></input>
+                    <Field type="submit" value="submit"></Field>
                 </div>
-            </form>
-        </div>
+            </Form>
+        </Formik>
     )
 }
 
