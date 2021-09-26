bc0bd67fa9d983b85be03672b19c73f3592f83d8
senal <ranga.fernando@gmail.com>
2021-09-26 19:57:00 +1000

    Displaying error in ErrorMessage component either with custom component or with children props

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+import TextError from './TextError';
+
 function YoutubeForm() {
     
     const initialValues = {
         name: '',
         email: '',
-        channel: ''
+        channel: '',
+        comments: '',
+        address: ''
     }
 
     const handleSubmit = (formData) => {
@@ -18,6 +22,7 @@ function YoutubeForm() {
         name: Yup.string().required('Required'),
         email: Yup.string().email('Invalid email format').required('Required'),
         channel: Yup.string().required('Required'),
+        address: Yup.string().required('Required')
     });
 
     return (
@@ -29,21 +34,53 @@ function YoutubeForm() {
             <Form >
                 <div className="form-control">
                     <label htmlFor="name">Name</label>
-                    <Field type="text" id="name" name="name" ></Field>
-                    <ErrorMessage className='error' name='name'></ErrorMessage>
+                    <Field type="text" id="name" name="name" placeholder="Enter name"></Field>
+                    <ErrorMessage className='error' name='name' component={TextError} ></ErrorMessage>
                 </div>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
-                    <Field type="email"  id="email" name="email"></Field>
-                    <ErrorMessage name='email'></ErrorMessage>
+                    <Field type="email"  id="email" name="email" placeholder="Enter your email"></Field>
+                    <ErrorMessage name='email'>
+                        {
+                            (errorMessage) => <div className='error'>{errorMessage}</div>
+                        }
+                    </ErrorMessage>
                 </div>
                 <div className="form-control">
                     <label htmlFor="channel">Channel</label>
-                    <Field type="text" id="channel" name="channel"></Field>
-                    <ErrorMessage name='channel'></ErrorMessage>
+                    <Field type="text" id="channel" name="channel" placeholder="Channel name"></Field>
+                    <ErrorMessage name='channel' component={TextError}></ErrorMessage>
                 </div>
                 <div className="form-control">
-                    <Field type="submit" value="submit"></Field>
+                    <label htmlFor="comments">Comments</label>
+                    <Field as='textarea' id="comments" name="comments" placeholder="Comments"></Field>
+                    <ErrorMessage name='channel'>
+                        {
+                            (errorMessage) => <div className='error'>{errorMessage}</div>
+                        }
+                    </ErrorMessage>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="address">Address</label>
+                    <Field name="address">
+                        {
+                         props => {
+                            const {form, field, meta} = props;
+
+                                return (
+                                    <div>
+                                        <input type='text' {...field} id="address" />
+                                        {meta.touched && meta.error ? <div className='error'>{meta.error}</div> : null}
+                                    </div>
+                                )
+                            }
+                        }
+                    </Field>
+                    <ErrorMessage name='channel'></ErrorMessage>
+                </div>
+
+                <div className="form-control">
+                    <Field type="submit" value="submit" ></Field>
                 </div>
             </Form>
         </Formik>
